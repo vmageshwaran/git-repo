7e6dd48dda63b51d78ea9f7bca44b2938cec50d7
vmageshwaran <vmageshwaran@yahoo.com>
2014-10-26 23:57:17 +0800

    some message goes here

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,6 @@
+# MakeCache Matrix - R Script
+# This function creates a special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -10,4 +13,18 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get,
        setmatrix = setmatrix,
        getmatrix = getmatrix)
+}
+
+# cacheSolve - R function
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+cacheSolve <- function(x, ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
